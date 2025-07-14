4737f7929abe4227d5309d4cee6b83a2db6db74f
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2025-07-14 21:11:27 +0530

    modified:   packages/requirement.txt 	modified:   sage/CMakeLists.txt 	modified:   sage/src/main.cpp

--- sage/src/main.cpp
+++ sage/src/main.cpp
@@ -1,11 +1,21 @@
 //Auto Genrated C++ file by Flick CLI
 //None
-#include<iostream>
+
+#include <fmt/base.h>
 #include<sageconfig.h>
 #include<fmt/core.h>
 #include<fmt/color.h>
+#include<cpr/cpr.h>
+#include<reproc++/reproc.hpp>
+
 int main(int argc,char*argv[])
 {
+  cpr::Response res=cpr::Get(cpr::Url("www.google.com"));
+  if(res.status_code==200){
+    fmt::println("ok!");
+  }else{
+    fmt::println("not ok!");
+  }
   fmt::print(fmt::emphasis::bold|fmt::fg(fmt::color::blue),"{} ",Project::PROJECT_NAME);
   fmt::print(fmt::emphasis::faint|fmt::fg(fmt::color::white),"v{}\n",Project::VERSION_STRING);
   return 0;
